92bbcb330bdc8812b8c5203fd0d09b8d04438e1d
ofi5 <aafaque123@yahoo.com>
2025-09-15 05:48:14 -0700

    prod changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, Button, Typography, Container, CssBaseline } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Home from "./pages/Home";
